ccf78e550fffef37cc01ef6b661b4cb5cdaf9540
Enis Afgan <afgane@gmail.com>
2022-07-08 09:41:06 -0400

    List individual tools in the readme

--- .github/scripts/update_readme.py
+++ .github/scripts/update_readme.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 import json
 import sys
+import yaml
 
 from jinja2 import Template
 
@@ -28,13 +29,30 @@ with open(".github/templates/README.md.j2", "r") as f:
     template = Template(f.read())
 
 with open(readme_path, "w") as f:
-    htmlout = "<thead><tr><th>Chunk ID</th><th>Tool List</th><th>Latest report</th><th>Previous report</th></tr></thead><tbody>"
     ids = list(chunks.keys())
     ids = [int(x) for x in ids]
     ids.sort()
     ids = [str(x) for x in ids]
+    # Write the chunked test reports table
+    reports_html = "<thead><tr><th>Chunk ID</th><th>Tool List</th><th>Latest report</th><th>Previous report</th></tr></thead><tbody>"
     for eachid in ids:
         eachchunk = chunks.get(eachid)
-        htmlout += "<tr><td>{}</td><td><a href=\"{}\">Toolset</a></td><td><a href=\"{}\">{}</a></td><td><a href=\"{}\">{}</a></td></tr>".format(eachid, eachchunk.get("tools"), eachchunk.get("run1"), eachchunk.get("date1"), eachchunk.get("run2"), eachchunk.get("date2"))
-    htmlout += "</tbody>"
-    f.write(template.render(anviltools=htmlout, reportdir=readme_path.replace("/README.md", "")))
+        reports_html += "<tr><td>{}</td><td><a href=\"{}\">Toolset</a></td><td><a href=\"{}\">{}</a></td><td><a href=\"{}\">{}</a></td></tr>".format(
+            eachid, eachchunk.get("tools"), eachchunk.get("run1"), eachchunk.get("date1"), eachchunk.get("run2"), eachchunk.get("date2"))
+    reports_html += "</tbody>"
+    # List tools individually
+    tool_tests_html = "<thead<tr><th>#</th><th>Tool ID</th><th>Test results</th></thead><tbody>"
+    count = 0
+    for each_id in ids:
+        chunk = chunks.get(each_id)
+        chunk_tools_url = chunk.get('tools')
+        chunk_tools_path = chunk_tools_url.replace('https://github.com/anvilproject/galaxy-tests/blob/main/', '')
+        with open(chunk_tools_path, 'r') as tf:
+            chunk_tools = yaml.safe_load(tf)
+            for tool in chunk_tools['tools']:
+                for rev in tool['revisions']:
+                    count += 1
+                    tool_tests_html += f"<tr><td>{count}</td><td>{tool['owner']}/{tool['name']}@{rev}</td><td></td></tr>"
+    tool_tests_html += "</tbody>"
+
+    f.write(template.render(toolreports=reports_html, tooltests=tool_tests_html, reportdir=readme_path.replace("/README.md", "")))
