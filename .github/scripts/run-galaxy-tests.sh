2069066112eab42e19bcac327b9e1443b632905e
Keith Suderman <suderman@jhu.edu>
2023-06-12 22:03:18 -0400

    Debugging run-galaxy-tests

--- .github/scripts/run-galaxy-tests.sh
+++ .github/scripts/run-galaxy-tests.sh
@@ -6,12 +6,13 @@ if [[ $# != 2 ]] ; then
   exit 1
 fi
 
-OUTDIR=$1
+HISTORY=$1
+OUTDIR=$2
 CHUNK_FILE=$OUTDIR/chunk.txt
 TIMESTAMP=$(date "+%F-%H-%M")
 
-history="anvil-test-$TIMESTAMP"
-echo "History is $history"
+#history="anvil-test-$TIMESTAMP"
+#echo "History is $history"
 cat << EOF > $OUTDIR/results.json
 {
   "tests": []
@@ -24,10 +25,10 @@ while read tool_args ; do
     -k $GALAXY_KEY \
     -a $GALAXY_KEY \
     -j $OUTDIR/results.json \
-    --history-name $history \
+    --history-name $HISTORY \
     --no-history-cleanup \
     --verbose \
     --append \
     $tool_args
 done < $CHUNK_FILE
-echo "history=$history"
+echo "history=$HISTORY"
